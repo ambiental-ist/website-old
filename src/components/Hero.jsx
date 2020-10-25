5e27aa1d26ed89bb022f9b6a2586e4432ef44e6d
PPSantos <pedro.pinto.santos@tecnico.ulisboa.pt>
2020-10-25 01:39:32 +0100

    Edited home page main text and link colors.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,15 +2,8 @@ import React from "react"
 
 const Hero = () => (
   <div className="container py-12 lg:pb-16">
-    <h2 className="text-3xl sm:text-4xl font-extrabold leading-tight tracking-tight text-gray-900">
-      Hello, I'm John{" "}
-      <span role="img" aria-label="waving hand">
-        👋
-      </span>
-      <br />
-      <span className="text-blue-600">
-        Welcome to my photography portfolio.
-      </span>
+    <h2 className="text-3xl sm:text-4xl font-extrabold leading-tight tracking-tight text-gray-900 text-center">
+      Grupo de estudantes <span className="green-color">ambientalista</span> do Instituto Superior Técnico.
     </h2>
   </div>
 )
