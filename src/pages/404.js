72f5d75f84beeb42a763e339300ef06715ab9e97
PPSantos <pedro.pinto.santos@tecnico.ulisboa.pt>
2021-01-03 20:05:00 +0000

    404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -10,11 +10,10 @@ const ErrorPage = () => (
       <div className="container py-12 pb-64 lg:pb-128">
         <div className="w-full md:w-1/2 xl:w-3/5 pb-8 md:pb-0">
           <h1 className="text-3xl leading-tight font-extrabold tracking-tight text-gray-800 sm:text-4xl">
-            Error 404
+            Ooops
           </h1>
-
-          <h2 className="text-xl leading-tight font-semibold tracking-tight text-blue-600 sm:text-2xl">
-            The page doesn't exists.
+          <h2 className="text-xl pt-4 leading-tight font-semibold tracking-tight text-gray-600 sm:text-2xl">
+            Página não encontrada.
           </h2>
         </div>
       </div>
